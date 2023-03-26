6c732e4a17cc19f9ae522b18163b279a6a9db948
Riotouschillum <vumailinht2@gmail.com>
2023-03-26 19:01:20 +0700

    Update rolling_sum_wo_consecutive_days.sql

--- rolling_sum_wo_consecutive_days.sql
+++ rolling_sum_wo_consecutive_days.sql
@@ -3,16 +3,15 @@ with t1 as (
     , co_name
     , sum(pv) as pv
     from clicks
-    where co_name = 'France'
     group by 1, 2
 )
 select t1.datecl
 , t1.co_name
 , sum(case when t2.datecl = t1.datecl then t2.pv end) as pv
-, sum(case when t2.datecl between t1.datecl - interval 3 day and t1.datecl then t2.pv end) as pv_3day
+, sum(case when t2.datecl between t1.datecl - interval 6 day and t1.datecl then t2.pv end) as pv_7day
+, sum(t2.pv) as pv_30day
 from t1
-left join t1 as t2 on ((t2.datecl between t1.datecl - interval 7 day and t1.datecl) and t1.co_name = t2.co_name)
-# where t1.co_name = 'France'
+left join t1 as t2 on ((t2.datecl between t1.datecl - interval 29 day and t1.datecl) and t1.co_name = t2.co_name)
 group by 1,2
 order by 1
 
